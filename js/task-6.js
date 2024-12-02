7f9ace0f4e2450c2c4462dc67a476fc7d46d7499
Polina Petrenko <ppetrenko2002@gmail.com>
2024-12-02 18:48:37 +0100

    hw-7.01

--- js/task-6.js
+++ js/task-6.js
@@ -4,37 +4,41 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const input = document.querySelector('#controls input');
-    const createButton = document.querySelector('[data-create]');
-    const destroyButton = document.querySelector('[data-destroy]');
-    const boxesContainer = document.querySelector('#boxes');
+document.addEventListener("DOMContentLoaded", function() {
+  const input = document.querySelector('#controls input');
+  const createButton = document.querySelector('[data-create]');
+  const destroyButton = document.querySelector('[data-destroy]');
+  const boxesContainer = document.querySelector('#boxes');
 
-    createButton.addEventListener('click', function() {
-      const amount = parseInt(input.value);
-      if (amount >= 1 && amount <= 100) {
-        createBoxes(amount);
-        input.value = '';
-      }
-    });
-
-    destroyButton.addEventListener('click', function() {
-      destroyBoxes();
-    });
-
-    function createBoxes(amount) {
-      destroyBoxes();
-      for (let i = 0; i < amount; i+=1) {
-        const box = document.createElement('div');
-        const size = 30 + i * 10;
-        box.style.width = `${size}px`;
-        box.style.height = `${size}px`;
-        box.style.backgroundColor = getRandomHexColor();
-        boxesContainer.appendChild(box);
-      }
-    }
-
-    function destroyBoxes() {
-      boxesContainer.innerHTML = '';
+  createButton.addEventListener('click', function() {
+    const amount = parseInt(input.value);
+    if (amount >= 1 && amount <= 100) {
+      createBoxes(amount);
+      input.value = '';
     }
   });
+
+  destroyButton.addEventListener('click', function() {
+    destroyBoxes();
+  });
+
+  function createBoxes(amount) {
+    destroyBoxes(); 
+    const fragment = document.createDocumentFragment(); 
+
+    for (let i = 0; i < amount; i += 1) {
+      const box = document.createElement('div');
+      const size = 30 + i * 10; 
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      fragment.appendChild(box); 
+    }
+
+    boxesContainer.appendChild(fragment); 
+  }
+
+  function destroyBoxes() {
+    boxesContainer.innerHTML = ''; 
+  }
+});
